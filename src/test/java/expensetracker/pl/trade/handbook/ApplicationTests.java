522d7d25639ac30dd23b1df35f16284034a3de97
Prost3333 <dimabolshoy33@gmail.com>
2026-07-10 12:48:05 +0200

    first commit

---
+++ src/test/java/expensetracker/pl/trade/handbook/ApplicationTests.java
@@ -0,0 +1,13 @@
+package expensetracker.pl.trade.handbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
